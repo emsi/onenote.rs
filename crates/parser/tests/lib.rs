0d5d0d140188de7c527d76f6812759c492876aa0
Markus Siemens <markus.siemens@vivavis.com>
2026-01-19 12:31:41 +0100

    refator: use yare for parameterizied tests

--- crates/parser/tests/lib.rs
+++ crates/parser/tests/lib.rs
@@ -1,6 +1,7 @@
 use insta::assert_debug_snapshot;
 use onenote_parser::Parser;
 use std::path::{Path, PathBuf};
+use yare::parameterized;
 
 #[test]
 fn test_parse_section() {
@@ -52,45 +53,36 @@ fn test_onenote_2016_parse_notebook() {
     assert_debug_snapshot!(parser.parse_section(path).unwrap())
 }
 
-#[test]
-fn test_onenote_joplin_examples() {
+#[parameterized(
+    checkboxes_and_unicode = { "checkboxes_and_unicode.one" },
+    math = { "Math.one" },
+    onenote_desktop = { "onenote_desktop.one" },
+    quick_notes = { "aaa/Quick Notes.one" },
+    audio_test = { "audio-test/Quick Notes.one" },
+    hyperlink_is_broken = { "hyperlink_is_broken/Quick Notes.one" },
+    subsections_subpages = { "Notebook with subsections and subpages/Section 1.one" },
+    notebook_with_chinese_char_on_link = { "notebook_with_chinese_char_on_link/Quick Notes.one" },
+    simple_notebook = { "Simple notebook/Quick Notes.one" },
+    new_section = { "new_section.one" }
+)]
+fn test_onenote_joplin_examples_section(path: &str) {
     let parser = Parser::new();
-    fn assert_section_snapshot(parser: &Parser, path: &str) {
-        assert_debug_snapshot!(parser.parse_section(Path::new(path)).unwrap());
-    }
+    assert_debug_snapshot!(
+        parser
+            .parse_section(Path::new(&*format!("tests/samples/joplin/{path}")))
+            .unwrap()
+    );
+}
 
-    fn assert_notebook_snapshot(parser: &Parser, path: &str) {
-        assert_debug_snapshot!(parser.parse_notebook(Path::new(path)).unwrap());
-    }
-
-    assert_section_snapshot(&parser, "tests/samples/joplin/checkboxes_and_unicode.one");
-    assert_section_snapshot(&parser, "tests/samples/joplin/Math.one");
-    assert_section_snapshot(&parser, "tests/samples/joplin/onenote_desktop.one");
-    assert_section_snapshot(&parser, "tests/samples/joplin/aaa/Quick Notes.one");
-    assert_section_snapshot(&parser, "tests/samples/joplin/audio-test/Quick Notes.one");
-    assert_notebook_snapshot(
-        &parser,
-        "tests/samples/joplin/default-edited/Открыть записную книжку.onetoc2",
+#[parameterized(
+    default_edited = { "default-edited/Открыть записную книжку.onetoc2" },
+    onenote_app = { "Notebook created on OneNote App/Abrir Bloco de Anotações.onetoc2" }
+)]
+fn test_onenote_joplin_examples_notebook(path: &str) {
+    let parser = Parser::new();
+    assert_debug_snapshot!(
+        parser
+            .parse_notebook(Path::new(&*format!("tests/samples/joplin/{path}")))
+            .unwrap()
     );
-    assert_section_snapshot(
-        &parser,
-        "tests/samples/joplin/hyperlink_is_broken/Quick Notes.one",
-    );
-    assert_notebook_snapshot(
-        &parser,
-        "tests/samples/joplin/Notebook created on OneNote App/Abrir Bloco de Anotações.onetoc2",
-    );
-    assert_section_snapshot(
-        &parser,
-        "tests/samples/joplin/Notebook with subsections and subpages/Section 1.one",
-    );
-    assert_section_snapshot(
-        &parser,
-        "tests/samples/joplin/notebook_with_chinese_char_on_link/Quick Notes.one",
-    );
-    assert_section_snapshot(
-        &parser,
-        "tests/samples/joplin/Simple notebook/Quick Notes.one",
-    );
-    assert_section_snapshot(&parser, "tests/samples/joplin/new_section.one");
 }
